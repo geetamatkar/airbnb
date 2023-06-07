101694a6a103a2d6f152d3b0b9a7bdc6815623d4
geetamatkar <geetamatkar1@gmail.com>
2023-06-07 14:40:44 -0500

    #Created async function for Server Side Rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 
+
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+
+  const reply = await getData();
+  console.log(reply)
   return (
     <div className=''>
       <Head> 
@@ -16,7 +20,36 @@ export default function Home() {
       {/*Banner*/}
       <Banner />
 
-    </div>
-    
-  )
+      <main className='max-w-5xl mx-auto px-8 sm:px-16'>
+      
+        <section className='pt-5'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull data from server */}
+          {reply?.map(
+							({
+								img,
+								location,
+								distance,
+							}: {
+								img: string;
+								location: string;
+								distance: string;
+							}) => (
+								<h2>{location}</h2>
+							)
+						)}
+          
+        </section>
+      </main>
+    </div>    
+  ) 
+}
+
+export async function getData() {
+
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G');
+
+  return exploreData.json()
+  
 }
--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const nextConfig = {}
 //module.exports = nextConfig
 module.exports ={
     images : {
-        domains: ['links.papareact.com', 'c4.wallpaperflare.com']
+        domains: ['links.papareact.com', 'wallpaperaccess.com']
     }
 }
